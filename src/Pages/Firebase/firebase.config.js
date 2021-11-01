21b884936937c7a96eb860fbd82d6fea5de0e713
Mohammad Mostafa <mohammad-mostafa94>
2021-11-01 08:11:03 +0600

    .env.local added

--- src/Pages/Firebase/firebase.config.js
+++ src/Pages/Firebase/firebase.config.js
@@ -1,10 +1,24 @@
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREBASE_APP_ID
+
+    // apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId:process.env.REACT_APP_FIREBASE_APP_ID
+
+    // REACT_APP_FIREBASE_API_KEY=AIzaSyDTjnVEmAyBEf50McehxGvAlFrVwQdl5Fo
+
+    // REACT_APP_FIREBASE_AUTH_DOMAIN=travel-house-c75b1.firebaseapp.com
+    // REACT_APP_FIREBASE_PROJECT_ID=travel-house-c75b1
+    
+    // REACT_APP_FIREBASE_STORAGE_BUCKET=travel-house-c75b1.appspot.com
+    
+    // REACT_APP_FIREBASE_MESSAGING_SENDER_ID=270811571851
+    
+    // REACT_APP_FIREBASE_APP_ID=1:270811571851:web:88b84c39be436c85ad968e
+    
+
 
     apiKey: "AIzaSyDTjnVEmAyBEf50McehxGvAlFrVwQdl5Fo",
     authDomain: "travel-house-c75b1.firebaseapp.com",
